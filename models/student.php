ada28395da11353da49b4b0d645d85b84ca39c93
Mohammed SHAMKHI <theshamkhi1@gmail.com>
2025-01-17 12:17:24 +0100

    Created joinCourse method for students to join courses

--- models/student.php
+++ models/student.php
@@ -8,8 +8,18 @@ class Student extends User {
         parent::__construct();
     }
 
-    public function joinCourse() {
-
+    public function joinCourse($courseID) {
+        try {
+            $studentID = $_SESSION['user_id'];
+            $query = "INSERT INTO Enrollments (CourseID, StudentID) 
+                      VALUES (:courseID, :studentID)";
+            $stmt = $this->connection->prepare($query);
+            $stmt->execute([':courseID' => $courseID, ':studentID' => $studentID]);
+    
+        } catch (PDOException $e) {
+            error_log($e->getMessage());
+            return "Failed to enroll: " . $e->getMessage();
+        }
     }
     public function getCourseDetails() {
 
