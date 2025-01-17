84ad36cddace6abe3185391cc73981f287c2728a
Mohammed SHAMKHI <theshamkhi1@gmail.com>
2025-01-17 17:12:40 +0100

    Created addCourse method in the teacher class

--- models/teacher.php
+++ models/teacher.php
@@ -8,7 +8,26 @@ class Teacher extends User {
         parent::__construct();
     }
 
-    public function addCourse() {
+    public function addCourse($title, $description, $media, $content, $category) {
+        try {
+            $teacherID = $_SESSION['user_id'];
+            $query = "INSERT INTO courses (Title, Description, Content, TeacherID, CatID, MediaURL) 
+                      VALUES (:title, :description, :content, :teacherID, :catID, :mediaURL)";
+            $stmt = $this->connection->prepare($query);
+    
+            $stmt->execute([
+                ':title' => $title,
+                ':description' => $description,
+                ':content' => $content,
+                ':teacherID' => $teacherID,
+                ':catID' => $category,
+                ':mediaURL' => $media
+            ]);
+    
+        } catch (PDOException $e) {
+            error_log($e->getMessage());
+            return "Failed to create course: " . $e->getMessage();
+        }
 
     }
     public function modifyCourse() {
