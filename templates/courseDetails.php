5bac4afc8ad2ae722c8c1f7391eff3333173ba88
Mohammed SHAMKHI <theshamkhi1@gmail.com>
2025-01-18 20:38:33 +0100

    Course Details now is accessible for Student & Teacher

--- templates/courseDetails.php
+++ templates/courseDetails.php
@@ -7,7 +7,7 @@ require_once '../models/admin.php';
 
 session_start();
 
-if (!isset($_SESSION['user_id']) || !isset($_SESSION['role']) || $_SESSION['role'] !== 'Student') {
+if (!isset($_SESSION['user_id']) || !isset($_SESSION['role']) || $_SESSION['role'] == 'Admin') {
     header("Location: login.php");
     exit;
 }
@@ -57,38 +57,57 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 
 <aside id="default-sidebar" class="fixed top-0 left-0 z-40 w-80 h-screen transition-transform -translate-x-full sm:translate-x-0" aria-label="Sidebar">
     <div class="h-full overflow-y-auto bg-black">
-    <div class="flex flex-col space-y-2">
-        <img src="https://website-cdn.studysmarter.de/2022/12/Udemy-1536x864.png" alt="Photo" class="object-cover">
-        <div class="px-3 pt-4">
-            <h2 class="text-xl font-semibold text-white text-center uppercase mb-4"><?php echo $student['Name']; ?></h2>
-        </div>
-    </div>
-    <ul class="space-y-2 font-medium px-3 pb-4">
-            <li>
+      <div class="flex flex-col space-y-2">
+          <img src="https://website-cdn.studysmarter.de/2022/12/Udemy-1536x864.png" alt="Photo" class="object-cover">
+          <div class="px-3 pt-4">
+              <h2 class="text-xl font-semibold text-white text-center uppercase mb-4"><?php echo $student['Name']; ?></h2>
+          </div>
+      </div>
+      <ul class="space-y-2 font-medium px-3 pb-4">
+            <?php if ($_SESSION['role'] == 'Student'): ?>
+              <li>
                 <a href="studentDashboard.php" class="flex items-center p-2 text-white rounded-lg hover:bg-gray-100 hover:text-black group">
                     <svg class="flex-shrink-0 w-5 h-5 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 18 18">
                         <path d="M6.143 0H1.857A1.857 1.857 0 0 0 0 1.857v4.286C0 7.169.831 8 1.857 8h4.286A1.857 1.857 0 0 0 8 6.143V1.857A1.857 1.857 0 0 0 6.143 0Zm10 0h-4.286A1.857 1.857 0 0 0 10 1.857v4.286C10 7.169 10.831 8 11.857 8h4.286A1.857 1.857 0 0 0 18 6.143V1.857A1.857 1.857 0 0 0 16.143 0Zm-10 10H1.857A1.857 1.857 0 0 0 0 11.857v4.286C0 17.169.831 18 1.857 18h4.286A1.857 1.857 0 0 0 8 16.143v-4.286A1.857 1.857 0 0 0 6.143 10Zm10 0h-4.286A1.857 1.857 0 0 0 10 11.857v4.286c0 1.026.831 1.857 1.857 1.857h4.286A1.857 1.857 0 0 0 18 16.143v-4.286A1.857 1.857 0 0 0 16.143 10Z"/>
                     </svg>
                 <span class="ms-3">Dashboard</span>
                 </a>
-            </li>
-            <li>
-              <a href="myCourses.php" class="flex items-center p-2 text-white rounded-lg hover:bg-gray-100 hover:text-black group">
-                  <svg class="flex-shrink-0 w-5 h-5 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 17 20">
-                      <path d="M7.958 19.393a7.7 7.7 0 0 1-6.715-3.439c-2.868-4.832 0-9.376.944-10.654l.091-.122a3.286 3.286 0 0 0 .765-3.288A1 1 0 0 1 4.6.8c.133.1.313.212.525.347A10.451 10.451 0 0 1 10.6 9.3c.5-1.06.772-2.213.8-3.385a1 1 0 0 1 1.592-.758c1.636 1.205 4.638 6.081 2.019 10.441a8.177 8.177 0 0 1-7.053 3.795Z"/>
-                  </svg>
-              <span class="ms-3">My courses</span>
-              </a>
-            </li>
-            <li>
-                <a href="logout.php" class="flex items-center p-2 text-white rounded-lg hover:bg-gray-100 hover:text-black group">
-                <svg class="flex-shrink-0 w-5 h-5 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 18 16">
-                    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 8h11m0 0L8 4m4 4-4 4m4-11h3a2 2 0 0 1 2 2v10a2 2 0 0 1-2 2h-3"/>
-                </svg>
-                <span class="flex-1 ms-3 whitespace-nowrap">Log Out</span>
+              </li>
+              <li>
+                <a href="myCourses.php" class="flex items-center p-2 text-white rounded-lg hover:bg-gray-100 hover:text-black group">
+                    <svg class="flex-shrink-0 w-5 h-5 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 17 20">
+                        <path d="M7.958 19.393a7.7 7.7 0 0 1-6.715-3.439c-2.868-4.832 0-9.376.944-10.654l.091-.122a3.286 3.286 0 0 0 .765-3.288A1 1 0 0 1 4.6.8c.133.1.313.212.525.347A10.451 10.451 0 0 1 10.6 9.3c.5-1.06.772-2.213.8-3.385a1 1 0 0 1 1.592-.758c1.636 1.205 4.638 6.081 2.019 10.441a8.177 8.177 0 0 1-7.053 3.795Z"/>
+                    </svg>
+                <span class="ms-3">My courses</span>
                 </a>
-            </li>
-        </ul>
+              </li>
+            <?php elseif ($_SESSION['role'] == 'Teacher'): ?>
+              <li>
+                  <a href="teacherDashboard.php" class="flex items-center p-2 text-white rounded-lg hover:bg-gray-100 hover:text-black group">
+                      <svg class="flex-shrink-0 w-5 h-5 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 18 18">
+                          <path d="M6.143 0H1.857A1.857 1.857 0 0 0 0 1.857v4.286C0 7.169.831 8 1.857 8h4.286A1.857 1.857 0 0 0 8 6.143V1.857A1.857 1.857 0 0 0 6.143 0Zm10 0h-4.286A1.857 1.857 0 0 0 10 1.857v4.286C10 7.169 10.831 8 11.857 8h4.286A1.857 1.857 0 0 0 18 6.143V1.857A1.857 1.857 0 0 0 16.143 0Zm-10 10H1.857A1.857 1.857 0 0 0 0 11.857v4.286C0 17.169.831 18 1.857 18h4.286A1.857 1.857 0 0 0 8 16.143v-4.286A1.857 1.857 0 0 0 6.143 10Zm10 0h-4.286A1.857 1.857 0 0 0 10 11.857v4.286c0 1.026.831 1.857 1.857 1.857h4.286A1.857 1.857 0 0 0 18 16.143v-4.286A1.857 1.857 0 0 0 16.143 10Z"/>
+                      </svg>
+                  <span class="ms-3">Dashboard</span>
+                  </a>
+              </li>
+              <li>
+                <a href="teacherCourses.php" class="flex items-center p-2 text-white rounded-lg hover:bg-gray-100 hover:text-black group">
+                    <svg class="flex-shrink-0 w-5 h-5 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 17 20">
+                        <path d="M7.958 19.393a7.7 7.7 0 0 1-6.715-3.439c-2.868-4.832 0-9.376.944-10.654l.091-.122a3.286 3.286 0 0 0 .765-3.288A1 1 0 0 1 4.6.8c.133.1.313.212.525.347A10.451 10.451 0 0 1 10.6 9.3c.5-1.06.772-2.213.8-3.385a1 1 0 0 1 1.592-.758c1.636 1.205 4.638 6.081 2.019 10.441a8.177 8.177 0 0 1-7.053 3.795Z"/>
+                    </svg>
+                <span class="ms-3">My courses</span>
+                </a>
+              </li>
+          <?php endif; ?>
+          <li>
+              <a href="logout.php" class="flex items-center p-2 text-white rounded-lg hover:bg-gray-100 hover:text-black group">
+              <svg class="flex-shrink-0 w-5 h-5 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 18 16">
+                  <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 8h11m0 0L8 4m4 4-4 4m4-11h3a2 2 0 0 1 2 2v10a2 2 0 0 1-2 2h-3"/>
+              </svg>
+              <span class="flex-1 ms-3 whitespace-nowrap">Log Out</span>
+              </a>
+          </li>
+      </ul>
    </div>
 </aside>
 
@@ -176,12 +195,16 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
                 <h3 class="text-xl font-semibold mb-4">Course Overview</h3>
                 <p class="text-gray-600"><?php echo $course['Content']; ?></p>
             </div>
-            <div class="flex items-center justify-center space-x-2">
-                <form method="POST">
-                    <input type="hidden" name="course_id" value="<?php echo $course['CourseID']; ?>">
-                    <button type="submit" class="bg-blue-600 hover:bg-blue-700 text-white px-6 py-2 rounded shadow-lg transition">Enroll Now</button>
-                </form>
-            </div>
+            <?php if ($_SESSION['role'] == 'Student'): ?>
+              <div class="flex items-center justify-center space-x-2">
+                  <form method="POST">
+                      <input type="hidden" name="course_id" value="<?php echo $course['CourseID']; ?>">
+                      <button type="submit" class="bg-blue-600 hover:bg-blue-700 text-white px-6 py-2 rounded shadow-lg transition">
+                          Enroll Now
+                      </button>
+                  </form>
+              </div>
+            <?php endif; ?>
         </div>
     </div>
 </div>
