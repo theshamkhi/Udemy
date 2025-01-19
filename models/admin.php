834b2c96f067f285f375244e01fd21ff884eaa2f
Mohammed SHAMKHI <theshamkhi1@gmail.com>
2025-01-19 17:48:55 +0100

    Added methods to create catergories & tags and another method to delete a category

--- models/admin.php
+++ models/admin.php
@@ -27,12 +27,34 @@ class Admin extends User {
             return "Failed to create category: " . $e->getMessage();
         }
     }
-    public function deleteCat() {
-
+    public function deleteCat($catID) {
+        try {
+            $query1 = "UPDATE Courses SET CatID = 5 WHERE CatID = :catID";
+            $stmt1 = $this->connection->prepare($query1);
+            $stmt1->execute([':catID' => $catID]);
+    
+            $query2 = "DELETE FROM Categories WHERE CatID = :catID";
+            $stmt2 = $this->connection->prepare($query2);
+            $stmt2->execute([':catID' => $catID]);
+        } catch (PDOException $e) {
+            throw new Exception("Database error: " . $e->getMessage());
+        }
     }
 
-    public function createTag() {
-
+    public function createTag($name) {
+        try {
+            $query = "INSERT INTO Tags (TagName) 
+                        VALUES (:name)";
+            $stmt = $this->connection->prepare($query);
+    
+            $stmt->execute([
+                ':name' => $name
+            ]);
+    
+        } catch (PDOException $e) {
+            error_log($e->getMessage());
+            return "Failed to create category: " . $e->getMessage();
+        }
     }
 }
 ?>
