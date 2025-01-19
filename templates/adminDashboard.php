b7ab4a68827c324251fc65d6334fa5aa98959363
Mohammed SHAMKHI <theshamkhi1@gmail.com>
2025-01-19 21:08:17 +0100

    Little enhancements

--- templates/adminDashboard.php
+++ templates/adminDashboard.php
@@ -66,21 +66,21 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['tag'])) {
 </button>
 
 <aside id="default-sidebar" class="fixed top-0 left-0 z-40 w-80 h-screen transition-transform -translate-x-full sm:translate-x-0" aria-label="Sidebar">
-    <div class="h-full overflow-y-auto bg-black">
-    <div class="flex flex-col space-y-2">
+  <div class="h-full overflow-y-auto bg-black">
+    <div class="flex flex-col space-y-2 mb-8">
         <img src="https://website-cdn.studysmarter.de/2022/12/Udemy-1536x864.png" alt="Photo" class="object-cover">
         <div class="px-3 pt-4">
-            <h2 class="text-xl font-semibold text-white text-center uppercase mb-4"><?php echo $admin['Username']; ?></h2>
+            <h2 class="text-xl font-semibold text-white text-center"><?php echo $admin['Username']; ?></h2>
         </div>
     </div>
     <ul class="space-y-2 font-medium px-3 pb-4">
             <li>
-                <a href="adminDashboard.php" class="flex items-center p-2 text-white rounded-lg hover:bg-gray-100 hover:text-black group">
-                    <svg class="flex-shrink-0 w-5 h-5 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 18 18">
-                        <path d="M6.143 0H1.857A1.857 1.857 0 0 0 0 1.857v4.286C0 7.169.831 8 1.857 8h4.286A1.857 1.857 0 0 0 8 6.143V1.857A1.857 1.857 0 0 0 6.143 0Zm10 0h-4.286A1.857 1.857 0 0 0 10 1.857v4.286C10 7.169 10.831 8 11.857 8h4.286A1.857 1.857 0 0 0 18 6.143V1.857A1.857 1.857 0 0 0 16.143 0Zm-10 10H1.857A1.857 1.857 0 0 0 0 11.857v4.286C0 17.169.831 18 1.857 18h4.286A1.857 1.857 0 0 0 8 16.143v-4.286A1.857 1.857 0 0 0 6.143 10Zm10 0h-4.286A1.857 1.857 0 0 0 10 11.857v4.286c0 1.026.831 1.857 1.857 1.857h4.286A1.857 1.857 0 0 0 18 16.143v-4.286A1.857 1.857 0 0 0 16.143 10Z"/>
-                    </svg>
-                <span class="ms-3">Dashboard</span>
-                </a>
+              <a href="adminDashboard.php" class="flex items-center p-2 text-white rounded-lg hover:bg-gray-100 hover:text-black group">
+                  <svg class="flex-shrink-0 w-5 h-5 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 18 18">
+                      <path d="M6.143 0H1.857A1.857 1.857 0 0 0 0 1.857v4.286C0 7.169.831 8 1.857 8h4.286A1.857 1.857 0 0 0 8 6.143V1.857A1.857 1.857 0 0 0 6.143 0Zm10 0h-4.286A1.857 1.857 0 0 0 10 1.857v4.286C10 7.169 10.831 8 11.857 8h4.286A1.857 1.857 0 0 0 18 6.143V1.857A1.857 1.857 0 0 0 16.143 0Zm-10 10H1.857A1.857 1.857 0 0 0 0 11.857v4.286C0 17.169.831 18 1.857 18h4.286A1.857 1.857 0 0 0 8 16.143v-4.286A1.857 1.857 0 0 0 6.143 10Zm10 0h-4.286A1.857 1.857 0 0 0 10 11.857v4.286c0 1.026.831 1.857 1.857 1.857h4.286A1.857 1.857 0 0 0 18 16.143v-4.286A1.857 1.857 0 0 0 16.143 10Z"/>
+                  </svg>
+              <span class="ms-3">Dashboard</span>
+              </a>
             </li>
             <li>
               <a href="accountsManager.php" class="flex items-center p-2 text-white rounded-lg hover:bg-gray-100 hover:text-black group">
@@ -134,12 +134,12 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['tag'])) {
 </div>
 
 <div class="flex-1 ml-0 sm:ml-80 p-8">
-    <h1 class="text-5xl font-semibold text-black mb-10">Categories</h1>
+    <h1 class="text-4xl font-semibold text-black mb-10">Categories</h1>
 
     <div class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4" style="align-items: start;">
       <?php foreach ($categories as $category): ?>
         <article class="overflow-hidden rounded-lg shadow transition hover:shadow-lg" data-aos="fade-up" data-aos-anchor-placement="top-bottom">
-            <div class="flex justify-between bg-white p-4 sm:p-6">
+            <div class="flex justify-between bg-white p-4">
                 <a href="#">
                     <h3 class="mt-0.5 text-lg text-gray-900">
                         <?php echo htmlspecialchars($category['CatName']); ?>
@@ -176,7 +176,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['tag'])) {
   </form>
 </div>
 <div class="flex-1 ml-0 sm:ml-80 p-8">
-    <h1 class="text-5xl font-semibold text-gray-800 mb-10">Tags</h1>
+    <h1 class="text-4xl font-semibold text-black mb-10">Tags</h1>
     <div class="grid grid-cols-2 sm:grid-cols-4 lg:grid-cols-6 gap-4">
         <?php foreach ($tags as $tag): ?>
             <span class="bg-gradient-to-r from-blue-500 to-blue-700 text-white text-sm px-4 py-2 rounded-lg shadow-md hover:shadow-lg transition-shadow duration-300 ease-in-out transform hover:-translate-y-1 cursor-pointer">
