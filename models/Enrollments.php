0cf00f787c2139a6694111877e165201a65af1cb
Mohammed SHAMKHI <theshamkhi1@gmail.com>
2025-01-20 21:03:53 +0100

    Added the other classes

---
+++ models/Enrollments.php
@@ -0,0 +1,9 @@
+<?php
+require_once '../config/db.php';
+
+
+class Enrollments {
+    private int $courseID;
+    private int $studentID;
+
+}
---
+++ models/category.php
@@ -0,0 +1,9 @@
+<?php
+require_once '../config/db.php';
+
+
+class Category {
+    private $catID;
+    private $catName;
+
+}
