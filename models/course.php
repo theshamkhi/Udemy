e46b95465251b0886baee7d927c445fae4e42781
Mohammed SHAMKHI <theshamkhi1@gmail.com>
2025-01-21 01:09:44 +0100

    Applied Polymorphisme to displayMedia function

--- models/course.php
+++ models/course.php
@@ -9,4 +9,35 @@ abstract class Course {
     protected $content;
     protected $mediaURL;
 
+    public function __construct($mediaURL) {
+        $this->mediaURL = $mediaURL;
+    }
+
+    abstract public function displayMedia();
 }
+
+class VideoCourse extends Course {
+
+    public function displayMedia() {
+        $embedURL = preg_replace(
+            '/https?:\/\/www\.youtube\.com\/watch\?v=([a-zA-Z0-9_-]+)/',
+            'https://www.youtube.com/embed/$1',
+            $this->mediaURL
+        );
+        return "<iframe width='560' height='315' src='$embedURL' frameborder='0' allowfullscreen></iframe>";
+    }
+    
+}
+
+class DocumentCourse extends Course {
+
+    public function displayMedia() {
+        return '<iframe src="' . $this->mediaURL . '" width="100%" height="600" style="border: none;"></iframe>';
+    }
+    
+}
+
+
+
+
+
